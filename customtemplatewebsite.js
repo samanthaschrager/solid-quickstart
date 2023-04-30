e961ea0321f05e0abc4d4fdc76ba19ddc6f4a040
samanthaschrager <132230979+samanthaschrager@users.noreply.github.com>
2023-04-30 18:32:42 -0400

    Add files via upload

---
+++ customtemplatewebsite.js
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Find the Beauty Within - Portfolio Template</title>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,700" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+     <div class="container">
+
+          <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="index.html" class="navbar-brand"><i class="fa fa-magnet"></i></a>
+          </div>
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">Projects</a></li>
+                    <li><a href="about.html">Our Studio</a></li>
+                    <li class="active"><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+               </ul>
+          </div>
+
+  </div>
+</div>
+
+<!-- Blog Section -->
+
+<section id="blog">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-1 col-md-10 col-sm-12">
+                    <div class="section-title">
+                         <h3>lastest Blogs</h3>
+                         <h2>The online hub for body and makeup/skin positivity, open for everyone.</h2>
+                    </div>
+
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-image">
+                              <a href="single-post.html">
+                                   <img src="images/blog-image1.jpg" class="img-responsive" alt="Blog Image">
+                              </a>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">We Help You Create A Better Internal Vision of You</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image1.jpg" class="img-responsive img-circle"> Jen Lopez</a></span>
+                              <span><i class="fa fa-date"></i> April 30, 2023</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 124 Comments</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>For more resources on this topic, literally look anywhere on the internet.</p>
+                              <a href="single-post.html" class="btn btn-default">Continue Reading</a>
+                         </div>
+                    </div>
+
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-image">
+                              <a href="single-post.html">
+                                   <img src="images/blog-image2.jpg" class="img-responsive" alt="Blog Image">
+                              </a>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">What is the purpose of this blog?</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image2.jpg" class="img-responsive img-circle"> Samantha Schrager</a></span>
+                              <span><i class="fa fa-date"></i> April 29, 2023</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 23 Comments</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>Hello World! This is a website/blog about how makeup does not have to be about covering up your "flaws," rather it can be about enhancing your natural beauty!.</p>
+                              <a href="single-post.html" class="btn btn-default">Continue Reading</a>
+                         </div>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Footer Section -->
+
+<footer>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-3 col-sm-3">
+                    <i class="fa fa-magnet"></i>
+               </div>
+
+               <div class="col-md-4 col-sm-4">
+                    <p>124 Market Street, Suite 3570 San Francisco, CA 3042 United States</p>
+               </div>
+
+               <div class="col-md-offset-1 col-md-4 col-sm-offset-1 col-sm-3">
+                    <p><a href="mailto:youremail@gmail.com">hello@thebeautywithin.co</a></p>
+                    <p>(+01) 2048937 / 02 203403</p>
+               </div>
+
+               <div class="clearfix col-md-12 col-sm-12">
+                    <hr>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="footer-copyright">
+                         <p>© 2016 Magnet Studio | All Rights Reserved.</p>
+                    </div>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <ul class="social-icon">
+                         <li><a href="#" class="fa fa-facebook"></a></li>
+                         <li><a href="#" class="fa fa-twitter"></a></li>
+                         <li><a href="#" class="fa fa-linkedin"></a></li>
+                    </ul>
+               </div>
+               
+          </div>
+     </div>
+</footer>
+
+
+<!-- SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
+
+
